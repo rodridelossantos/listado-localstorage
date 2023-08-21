c9fccebb0cd01b0179b9056f7051d4c0c806a0ea
rodridelossantos <137809941+rodridelossantos@users.noreply.github.com>
2023-08-21 17:56:55 -0300

    Limpiar y comentar código

--- js/almacenar.js
+++ js/almacenar.js
@@ -4,8 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const btnLimpiar = document.getElementById("limpiar");
   const contenedor = document.getElementById("contenedor");
 
+  // Si el item datos en localStorage existe, entonces se guarda en la variable, si no, se guarda un array vacío
   let arrayDatos = JSON.parse(localStorage.getItem("datos")) || [];
 
+  // Esta función agrega todos los elementos del localStorage al contenedor
   function addEntireArray(arr) {
     for (let i = 0; i < arr.length; i++) {
       let li = document.createElement("li");
@@ -16,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function displayAnElement(arr) {
-    //
+    // Adjunta un elemento al contenedor html
     contenedor.innerHTML += `<li class="list-group-item">${
       arr[arr.length - 1]
     } </li>`;
@@ -24,13 +26,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
   addEntireArray(arrayDatos);
 
+  // Este botón agrega el valor del input al array del localstorage y lo muestra en pantalla.
   btnAgregar.addEventListener("click", () => {
-    arrayDatos.push(item.value);
-    localStorage.setItem("datos", JSON.stringify(arrayDatos));
-    item.value = "";
-    displayAnElement(arrayDatos);
+    // Si el input tiene valor se muestra el elemento en pantalla
+    if (item.value) {
+      arrayDatos.push(item.value);
+      localStorage.setItem("datos", JSON.stringify(arrayDatos));
+      item.value = "";
+      displayAnElement(arrayDatos);
+    }
   });
 
+  // Quita la lista entera del contenedor al apretar en limpiar
   btnLimpiar.addEventListener("click", () => {
     localStorage.removeItem("datos");
     contenedor.textContent = "";
